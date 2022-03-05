5918f77322799b9d57a8d4c7852a923cce85f418
mayurjadhav09 <mayurjadhav270@gmail.com>
2022-03-05 19:29:42 +0530

    data fetching done

---
+++ pages/navBar.js
@@ -0,0 +1,40 @@
+
+export const navBar=()=>{
+
+    return (`  <div id="navContainer">
+    <div class="navcont_div">
+      <img class="navImg" src="./img/imgur-128.png" alt="" />
+    </div>
+    <div class="navcont_div">
+      <img
+        src="https://s.imgur.com/desktop-assets/desktop-assets/icon-new-post.13ab64f9f36ad8f25ae3544b350e2ae1.svg"
+        alt=""
+      /><button class="imgrbtn">New post</button>
+    </div>
+    <!-- ------------searhbar--------------------------------------------------- -->
+    <div class="searchBar">
+        <input type="text" placeholder="Images, #tags ,@users oh My!">
+        <img src="https://s.imgur.com/desktop-assets/desktop-assets/icon-search.8d0f9b564a4659d48d8eca38b968a7f2.svg" alt="">
+    </div>
+
+    <!-- ------------searhbar--------------------------------------------------- -->
+    <div class="navcont_div">   <img class="navImg"
+        src="./img/pngfind.com-weather-icons-png-2576117.png"
+        alt=""
+      /></div>
+    <div class="navcont_div">
+   
+      <button class="imgrbtn2" style="background-color: blueviolet;">Go Ad-Free</button>
+    </div>
+    <div class="navcont_div"><button style="background-color: transparent;" class="imgrbtn">Sign in</button></div>
+    <div class="navcont_div"><button style="width: 100%; height: 100%;" class="imgrbtn">Sign up</button></div>
+  </div>
+        
+    
+    `)
+
+
+}
+
+
+ 
